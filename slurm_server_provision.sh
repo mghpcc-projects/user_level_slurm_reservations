ea7309ca3bcad093595f7a19d968337195aaf3b7
Tim Donahue <tpd001@gmail.com>
2017-07-11 15:20:12 -0400

    Rename to prepare for Slurm controller provisioning script

--- slurm_server_provision.sh
+++ slurm_server_provision.sh
@@ -8,6 +8,7 @@
 #   Run as root on compute nodes
 #   Controller is provisioned separately
 #   NFS kernel server must be provisioned on the controller
+#      /slurm is the shared directory, exported via NFS
 #   Assumes Ubuntu environment (16.04 LTS, YMMV)
 
 set -x
@@ -37,7 +38,7 @@ wget https://www.gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.27.tar.bz2
 tar xvf libgpg-error-1.27.tar.bz2
 cd libgpg-error-1.27
 ./configure
-sudo make install
+make install
 cd 
 cd packages
 
@@ -45,14 +46,14 @@ wget https://www.gnupg.org/ftp/gcrypt/libgcrypt/libgcrypt-1.7.8.tar.bz2
 tar xvf libgcrypt-1.7.8.tar.bz2
 cd libgcrypt-1.7.8
 ./configure
-sudo make install
+make install
 cd 
 cd packages
 
 wget https://github.com/dun/munge/releases/download/munge-0.5.12/munge-0.5.12.tar.xz
 tar xvf munge-0.5.12.tar.xz
 ./config
-sudo make install
+make install
 cd 
 cd packages
 
@@ -60,12 +61,17 @@ wget https://github.com/SchedMD/slurm/archive/slurm-17-02-6-1.tar.gz
 tar xvf slurm-17-02-6-1.tar.gz
 cd slurm-slurm-17-02-6-1
 ./configure
-sudo make install
+make install
 
-systemctl enable slurmd
+# NFS
 
-sudo mkdir -p /local/slurm
-sudo mount 10.0.0.8:/slurm /local/slurm
+mkdir -p /local/slurm
+chmod 777 /local/slurm
+mount controller:/slurm /local/slurm
+echo "controller:/slurm /local/slurm nfs rsize=8192,wsize=8192,timeo=14,intr" >> /etc/fstab
 
 cd 
 cd packages
+
+systemctl enable slurmd
+
