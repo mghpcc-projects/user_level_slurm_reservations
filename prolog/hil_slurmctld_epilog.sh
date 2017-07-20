bbdd83d47ecb0112983645a0cf9fec1a2cfdd790
Tim Donahue <tpd001@gmail.com>
2017-07-20 13:51:18 -0400

    Update location of installed files

--- prolog/hil_slurmctld_epilog.sh
+++ prolog/hil_slurmctld_epilog.sh
@@ -4,12 +4,12 @@
 #
 # Runs hil_slurmctld_prolog.py with --hil_epilog, e.g. as the epilog
 #
-PATH=/bin:/usr/bin
+PATH=/bin:/usr/bin:/usr/local/bin
 LOGFILE=/var/log/slurm-llnl/hil_prolog.log
-HOME=/vagrant/user_level_slurm_reservations
+HOME=/home/slurm/scripts
 
 source ${HOME}/../ve/bin/activate
-python $HOME/prolog/hil_slurmctld_prolog.py --hil_epilog >> $LOGFILE 2>&1
+python ${HOME}/hil_slurmctld_prolog.py --hil_epilog >> $LOGFILE 2>&1
 deactivate
 
 exit 0
--- prolog/hil_slurmctld_prolog.sh
+++ prolog/hil_slurmctld_prolog.sh
@@ -6,10 +6,10 @@
 #
 PATH=/bin:/usr/bin
 LOGFILE=/var/log/slurm-llnl/hil_prolog.log
-HOME=/vagrant/user_level_slurm_reservations
+HOME=/home/slurm/scripts
 
 source ${HOME}/../ve/bin/activate
-python $HOME/prolog/hil_slurmctld_prolog.py --hil_prolog >> $LOGFILE 2>&1
+python ${HOME}/hil_slurmctld_prolog.py --hil_prolog >> $LOGFILE 2>&1
 deactivate
 
 exit 0
