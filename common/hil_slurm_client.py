cf9b7a55fed7c78a27f0c8f99fec48e4ff1b0a46
naved001 <naved001@gmail.com>
2017-09-19 17:13:06 -0400

    Refactoring

--- common/hil_slurm_client.py
+++ common/hil_slurm_client.py
@@ -14,7 +14,6 @@ from hil_slurm_logging import log_info, log_debug, log_error
 from hil_slurm_settings import HIL_ENDPOINT, HIL_USER, HIL_PW, HIL_SLURM_PROJECT
 
 
-
 def hil_client_connect(endpoint_ip, name, pw):
     '''
     '''
@@ -28,6 +27,21 @@ def check_hil_interface():
     hil_client = hil_init()
 
 
+def hil_move_node(node, dest_project, source_project=None):
+    hil_client = hil_init()
+    node_info = hil_client.node.show(node)
+
+    if source_project is None:
+        source_project = node_info['project']
+    else:
+        assert source_project is node_info['project'], "Project mismatch"
+
+    hil_client.node.power_off(node)
+    _remove_all_networks(node, hil_client)
+    hil_client.project.detach(source_project, node)
+    hil_client.project.connect(dest_project, node)
+
+
 def hil_reserve_nodes(nodelist, dest_project):
     '''
     Cause HIL nodes to move from the Slurm loaner project to a new HIL project.
@@ -39,21 +53,11 @@ def hil_reserve_nodes(nodelist, dest_project):
     network is also controlled by HIL. If we removed all networks, then we will
     not be able to perform any ipmi operations on nodes.
     '''
-    hil_client = hil_init()
     for node in nodelist:
-        # get information from node
-        node_info = hil_client.node.show(node)
-        project = node_info['project']
-        # check that the correct project is stored
-        assert project == HIL_SLURM_PROJECT
-        # prep and move the node to free pool
-        hil_client.node.power_off(node)
-        _remove_all_networks(node, hil_client)
-        hil_client.project.detach(project, node)
-        hil_client.project.connect(dest_project, node)
+        hil_move_node(node, dest_project, source_project=HIL_SLURM_PROJECT)
 
 
-def hil_free_nodes(nodelist, dest_project):
+def hil_free_nodes(nodelist):
     '''
     Cause HIL nodes to move from a HIL project to the Slurm loaner project.
 
@@ -64,17 +68,13 @@ def hil_free_nodes(nodelist, dest_project):
     network is also controlled by HIL. If we removed all networks, then we will
     not be able to perform any ipmi operations on nodes.
     '''
-    hil_client = hil_init()
     for node in nodelist:
-        # get information from node
-        node_info = hil_client.node.show(node)
-        project = node_info['project']
-        # check that the node is not in Slurm already
-        assert project != HIL_SLURM_PROJECT
-        # prep and return node to Slurm
-        hil_client.node.power_off(node)
-        _remove_all_networks(node, hil_client)
-        hil_client.project.connect(HIL_SLURM_PROJECT, node)
+        hil_move_node(node, HIL_SLURM_PROJECT)
+        # should probably atach the networks back for SLURM project
+        # 1. either copy the network config off of some other node in SLURM
+        # 2. reconnect every network in the SLURM PROJECT
+        # 3. hardcode some network names in a config file that needs to be
+        # reattached.
 
 
 def hil_init():
