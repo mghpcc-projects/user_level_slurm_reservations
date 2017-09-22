39b35afda7a5dc0e5b06dbd877a49497dc70b6c6
Tim Donahue <tpd001@users.noreply.github.com>
2017-09-22 09:31:33 -0400

    Move virtual env directory ve up and directly beneath $HOME

--- commands/hil_slurm_monitor.sh
+++ commands/hil_slurm_monitor.sh
@@ -3,7 +3,7 @@
 HOME=/vagrant/user_level_slurm_reservations
 LOGFILE=/var/log/slurm-llnl/hil_monitor.log
 
-source $HOME/../ve/bin/activate
+source $HOME/ve/bin/activate
 export PYTHONPATH=$HOME/prolog:$PYTHONPATH
 # python $HOME/commands/hil_slurm_monitor.py 2>&1 >> $LOGFILE
 python $HOME/commands/hil_slurm_monitor.py
