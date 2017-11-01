aad24add4601d2d8932c8df5dbe1662560c7b4e4
naved001 <naved001@gmail.com>
2017-11-01 12:37:12 -0400

    Refactor the HIL client

--- common/hil_slurm_client.py
+++ common/hil_slurm_client.py
@@ -14,6 +14,14 @@ from hil_slurm_logging import log_info, log_debug, log_error
 from hil_slurm_settings import HIL_ENDPOINT, HIL_USER, HIL_PW
 
 
+class HILClientFailure(Exception):
+    """Exception indicating that the HIL client failed"""
+
+
+class ProjectMismatchError(Exception):
+    """Raised when projects don't match"""
+
+
 def hil_client_connect(endpoint_ip, name, pw):
     '''
     Connect to the HIL server and return a HIL Client instance
@@ -37,53 +45,45 @@ def hil_reserve_nodes(nodelist, from_project, hil_client=None):
     Cause HIL nodes to move from the 'from' project to the HIL free pool.
     Typically, the 'from' project is the Slurm loaner project.
 
-    This methods first powers off the nodes, then disconnects all networks, 
+    This methods first powers off the nodes, then disconnects all networks,
     then moves the node from the 'from' project to the free pool.
 
     We power off the nodes before removing the networks because the IPMI
     network is also controlled by HIL. If we removed all networks, then we will
     not be able to perform any IPMI operations on nodes.
     '''
-    status = True
-
+    # this might cause a loop? since hil_init calls hil_client_connect
+    # which can return None. We can either have a counter to give it a
+    # reasonable number of tries before raising an exception.
     if not hil_client:
         hil_client = hil_init()
 
+    # Get information from node and ensure that the node is actually connected
+    # to <from_project> before proceeding.
     for node in nodelist:
-        # get information from node
-        try:
-            node_info = hil_client.node.show(node)
-        except:
-            log_error('HIL reservation failure: HIL node info unavailable, node `%s`' % node)
-            status = False
-            continue
+        node_info = show_node(hil_client, node)
 
         project = node_info['project']
         if (project != from_project):
             log_error('HIL reservation failure: Node `%s` (in project `%s`) not in `%s` project' % (node, project, from_project))
-            status = False
-            continue
+            raise ProjectMismatchError()
 
-        # prep and move the node from the Slurm project to the HIL free pool
-        try:
-            hil_client.node.power_off(node)
-        except:
-            log_error('HIL reservation failure: Unable to power off node `%s`' % node)
-            status = False
-            continue
+    # Power off all nodes.
+    for node in nodelist:
+        power_off_node(hil_client, node)
 
-        if not _remove_all_networks(node, hil_client):
-            status = False
-            continue
+    # Remove all networks from nodes.
+    for node in nodelist:
+        _remove_all_networks(hil_client, node)
 
+    # Finally, remove node from project.
+    for node in nodelist:
         try:
             hil_client.project.detach(from_project, node)
+            log_info('Node `%s` removed from project `%s`' % (node, from_project))
         except:
             log_error('HIL reservation failure: Unable to detach node `%s` from project `%s`' % (node, from_project))
-            status = False
-            continue
-
-    return status
+            raise HILClientFailure()
 
 
 def hil_free_nodes(nodelist, to_project, hil_client=None):
@@ -91,54 +91,45 @@ def hil_free_nodes(nodelist, to_project, hil_client=None):
     Cause HIL nodes to move the HIL free pool to the 'to' project.
     Typically, the 'to' project is the Slurm loaner project.
 
-    This method first powers off the nodes, then disconnects all networks, 
+    This method first powers off the nodes, then disconnects all networks,
     then moves the node from the free pool to the 'to' project.
 
     We power off the nodes before removing the networks because the IPMI
     network is also controlled by HIL. If we removed all networks, then we will
     not be able to perform any IPMI operations on nodes.
     '''
-    status = True
 
     if not hil_client:
         hil_client = hil_init()
 
+    # Get information from node and ensure that the node is actually connected
+    # to <from_project> before proceeding.
     for node in nodelist:
-        # get information from node
-        try:
-            node_info = hil_client.node.show(node)
-        except:
-            log_error('HIL release failure: HIL node info unavailable, node `%s`' % node)
-            status = False
-            continue
+        node_info = show_node(hil_client, node)
 
         # If the node is in the Slurm project now, skip further processing, but don't indicate
         # failure.
         project = node_info['project']
         if (project == to_project):
             log_info('HIL release: Node `%s` already in `%s` project, skipping' % (node, to_project))
-            continue
+            nodelist.remove(node)
 
-        # prep and move the node from the HIL free pool to the Slurm project
-        try:
-            hil_client.node.power_off(node)
-        except:
-            log_error('HIL release failure: Unable to power off node `%s`' % node)
-            status = False
-            continue
-            
-        if not _remove_all_networks(node, hil_client):
-            status = False
-            continue
+    # Power off all nodes.
+    for node in nodelist:
+        power_off_node(hil_client, node)
 
+    # Remove all networks from nodes.
+    for node in nodelist:
+        _remove_all_networks(hil_client, node)
+
+    # Finally, connect node to <to_project>
+    for node in nodelist:
         try:
             hil_client.project.connect(to_project, node)
+            log_info('Node `%s` connected to project `%s`' % (node, to_project))
         except:
             log_error('HIL reservation failure: Unable to connect node `%s` to project `%s`' % (node, to_project))
-            status = False
-            continue
-
-    return status
+            raise HILClientFailure()
 
 
 def hil_init():
@@ -149,13 +140,7 @@ def _remove_all_networks(node, hil_client):
     '''
     Disconnect all networks from all of the node's NICs
     '''
-    try:
-        node_info = hil_client.node.show(node)
-    except:
-        log_error('Failed to retrieve info for HIL node `%s`' % node)
-        return False
-
-    status = True
+    node_info = show_node(hil_client, node)
 
     # get node information and then iterate on the nics
     for nic in node_info['nics']:
@@ -165,11 +150,30 @@ def _remove_all_networks(node, hil_client):
         if port and switch:
             try:
                 hil_client.port.port_revert(switch, port)
+                log_info('Removed all networks from node `%s`' % node)
             except:
                 log_error('Failed to revert port `%s` on node `%s` switch `%s`' % (port, node, switch))
-                status = False
-                continue
+                raise HILClientFailure()
 
-    return status
 
-# EOF
+def show_node(hil_client, node):
+    """Returns node information and takes care of handling exceptions"""
+    try:
+        node_info = hil_client.node.show(node)
+        return node_info
+    except Exception as ex:
+        # log a note for the admins, and the exact exception before raising
+        # an error.
+        log_error('HIL reservation failure: HIL node info unavailable, node `%s`' % node)
+        log_error(ex)
+        raise HILClientFailure()
+
+
+def power_off_node(hil_client, node):
+    try:
+        hil_client.node.power_off(node)
+        log_info('Node `%s` succesfully powered off' % node)
+    except Exception as ex:
+        log_error('HIL reservation failure: Unable to power off node `%s`' % node)
+        log_error(ex)
+        raise HILClientFailure()
